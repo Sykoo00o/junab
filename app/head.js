0b005469fca9b788a8d124c6fcbf937d03163021
Sykoo00o <109036799+Sykoo00o@users.noreply.github.com>
2026-02-16 23:24:11 +0100

    Create head.js

---
+++ app/head.js
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.png" />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4421482351262183"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
+}
